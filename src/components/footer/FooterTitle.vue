295cb0aa2623871e86678cec7366b33ea1d01b07
Nasir Raza <nasirofficial074@gmail.com>
2023-04-04 01:47:23 +0500

    all rate pages added

--- src/components/footer/FooterTitle.vue
+++ src/components/footer/FooterTitle.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="py-[60px] flex flex-col items-center bg-bgImg bg-center bg-cover bg-no-repeat px-5" :class="bgBlue===true?'bg-blue':'bg-[#DDF1FA]'">
         <p class="font-light text-[32px] mb-8 text-center" :class="bgBlue===true?'text-white':'text-blue'" >{{title}}</p>
-        <router-link to="" :class="bgBlue===true?'btn-yellow':'btn-blue'">Sign Up Fro Free</router-link>
+        <router-link to="" :class="bgBlue===true?'btn-yellow':'btn-blue'">Sign Up for Free</router-link>
     </div>
 </template>
 
