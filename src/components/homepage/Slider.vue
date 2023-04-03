e7e1f583a6aa54e6212dfc68afe8a326af0dfb2a
Nasir Raza <nasirofficial074@gmail.com>
2023-04-03 21:37:01 +0500

    all rate pages added

--- src/components/homepage/Slider.vue
+++ src/components/homepage/Slider.vue
@@ -3,18 +3,22 @@
     <h2 class="text-blue font-medium px-5 text-4xl mb-12 text-center">What Our Customers Are Saying</h2>
     <div class="row">
         <div class="slider py-4 pb-12">
-            <div class="min-w-full flex px-5` grid grid-cols-2 justify-items-center gap-x-3 max-md:grid-cols-1" v-for="i in 3" :key="i">
-                <div class="relative w-full max-w-[80%] bg-white shadow-lg rounded-xl p-12">
+            <div class="min-w-full flex px-5` grid grid-cols-2 justify-items-center gap-x-3 max-md:grid-cols-1" v-for="(quote, i) in quotes" :key="i">
+                <div class="relative w-full max-w-[80%] bg-white shadow-lg rounded-xl p-12 flex flex-col justify-between">
                     <img src="@/assets/images/dblquote.png" alt="" class="w-12 absolute -top-2 -left-2">
-                    <h4 class="text-2xl leading-8 text-[#333333] mb-3">Lorem ipsum dolor sit amet, consectetur</h4>
-                    <p class="italic text-[#737373] mb-3">“Aliquam finibus nibh sit amet orci malesuada, a tempor mauris lobortis. Lorem ipsum dolor sit amet.”</p>
-                    <p class="font-semibold text-blue text-sm">Nicholas Look</p>
+                    <div>
+                        <h4 class="text-2xl leading-8 text-[#333333] mb-3">{{ quote[0].heading }}</h4>
+                        <p class="italic text-[#737373] mb-4 justify-items-start">{{ quote[0].description }}</p>
+                    </div>
+                    <p class="font-semibold text-blue text-sm">{{ quote[0].author }}</p>
                 </div>
-                <div class="relativew-full max-w-[80%] bg-white shadow-lg rounded-xl p-12 max-md:hidden">
+                <div class="relative w-full max-w-[80%] bg-white shadow-lg rounded-xl p-12 max-md:hidden flex flex-col justify-between">
                     <img src="@/assets/images/dblquote.png" alt="" class="w-12 absolute -top-2 -left-2">
-                    <h4 class="text-2xl leading-8 text-[#333333] mb-3">Id venenatis a condimentum vitae sapien</h4>
-                    <p class="italic text-[#737373]">I have been using Aeropost for years and what makes them great is the excellent customer service, both locally and also via online chat. I love their coupon program, the ability to have all-inclusive pricing of your items, and tracking. The shipping speed is also great and the easy return policy! I wouldn’t change a thing!”</p>
-                    <p class="font-semibold text-blue text-sm">Nicholas Look</p>
+                    <div>
+                        <h4 class="text-2xl leading-8 text-[#333333] mb-3">{{ quote[1].heading }}</h4>
+                        <p class="italic text-[#737373] mb-4">{{ quote[1].description }}</p>
+                    </div>
+                    <p class="font-semibold text-blue text-sm">{{ quote[1].author }}</p>
                 </div>
             </div>
         </div>
@@ -32,6 +36,46 @@ export default {
     data(){
         return{
             currentSlide : 0,
+            quotes:[
+                [
+                    {
+                        heading: 'Always reliable and efficient!', 
+                        description:'“Always reliable and efficient. They have a variety of options, which is commendable!”',
+                        author:'Stacy-Ann Allen'
+                    },
+                    {
+                        heading: 'Excellent Customer Service', 
+                        description:'“I have been using Aeropost for years and what makes them great is the excellent customer service, both locally and also via online chat. I love their coupon program, the ability to have all-inclusive pricing of your items, and tracking. The shipping speed is also great and the easy return policy! I wouldn’t change a thing!” ',
+                        author:'Lavahn Williams'
+                    },
+                    
+                ],
+                [
+                    {
+                        heading: 'On time, easy to track, and super affordable.', 
+                        description:'“The service is always an excellent experience; On time, easy to track, and super affordable. Thanks for the smooth operation every time.”',
+                        author:'Julian Jones'
+                    },
+                    {
+                        heading: 'Always reliable and efficient!', 
+                        description:'“Always reliable and efficient. They have a variety of options, which is commendable!”',
+                        author:'Stacy-Ann Allen'
+                    },
+                    
+                ],
+                [
+                    {
+                        heading: 'Excellent Customer Service', 
+                        description:'“I have been using Aeropost for years and what makes them great is the excellent customer service, both locally and also via online chat. I love their coupon program, the ability to have all-inclusive pricing of your items, and tracking. The shipping speed is also great and the easy return policy! I wouldn’t change a thing!” ',
+                        author:'Lavahn Williams'
+                    },
+                    {
+                        heading: 'On time, easy to track, and super affordable.', 
+                        description:'“The service is always an excellent experience; On time, easy to track, and super affordable. Thanks for the smooth operation every time.”',
+                        author:'Julian Jones'
+                    },
+                ]
+            ]
         }
     },
     methods:{
