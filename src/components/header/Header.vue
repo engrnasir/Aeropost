713fca9481d4099d3027e290c588e84ce798cfa6
Nasir Raza <nasirofficial074@gmail.com>
2023-04-25 22:32:26 +0500

    country picker fixed

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,16 +1,13 @@
 <template>
     <div class=" px-8 py-4">
-        <CountryPicker/>
         <Navigation/>
     </div>
 </template>
 
 <script>
-import CountryPicker from '@/components/header/CountryPicker.vue';
 import Navigation from '@/components/header/Navigation.vue'
 export default {
     components:{
-        CountryPicker,
         Navigation,
     },
 }
--- src/views/CountryPicker.vue
+++ src/views/CountryPicker.vue
@@ -5,7 +5,7 @@
             <img src="@/assets/images/x.png" alt="x" class="w-12 h-12 cursor-pointer absolute right-1 top-1 3xl:right-3 3xl:top-3">
         </router-link>
 
-        <div class="flex items-center justify-center mb-14 md:mb-20">
+        <div class="flex items-center justify-center mb-14 md:mb-20 lg:hidden">
             <router-link :to="`/${selectedCountry.gtw}/${lang}`">
                 <LogoVue :colorVal="'#ffffff'"/>
             </router-link>
