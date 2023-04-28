27dd9599dbb9fc20792d407444fb08a099b5c66e
Nasir Raza <nasirofficial074@gmail.com>
2023-04-28 21:08:07 +0500

    updating link

--- src/components/header/CountryControl.vue
+++ src/components/header/CountryControl.vue
@@ -26,9 +26,9 @@ export default {
         ...mapState([
             'lang',
             'showCountries',
-            'selectedCountry'
+            'selectedCountry',
         ]),
-    },
+    },  
     methods:{
         ...mapMutations([
             'setLang',
